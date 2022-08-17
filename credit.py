f4a3bea28448ad44f39ffe49ebdd286acc3781e4
Yuri Soares <yurisoaresm@outlook.com>
2022-08-17 13:54:26 -0300

    Rewritted idCard function

--- credit.py
+++ credit.py
@@ -4,16 +4,16 @@ def idCard (num):
     if str(num)[0] == '4':
         print('VISA')
     else:
-        char = str(num)[0] + str(num)[1]
-        if char == '34' or char == '37':
+        if str(num)[0:2] in ['34', '37']:
             print('AMEX')
-        elif char == '51' or char == '52' or char == '53' or char == '54' or char == '55':
+        elif str(num)[0:2] in ['51', '52', '53', '54', '55']:
             print('MASTERCARD')
 
 
 # Validar o número do cartão
 def validateCard (num):
     checkSum = 0
+    listNum = []
     count = len(num) - 2
     while count > 0:
         str(num)[count]
