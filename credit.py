e30d2d618d173e5692b9836c0ef5505fecc9c605
Yuri Soares <yurisoaresm@outlook.com>
2022-08-19 17:14:44 -0300

    Fixed validate_card function and added new comments

--- credit.py
+++ credit.py
@@ -1,5 +1,4 @@
-
-# Identificar o tipo do cartão
+# Função que identifica a bandeira do cartão (NOTA: apenas Visa, Mastercard ou American Express)
 def id_card(num):
     if str(num)[0] == '4':
         print('VISA\n')
@@ -8,27 +7,29 @@ def id_card(num):
             print('AMEX\n')
         elif str(num)[0:2] in ['51', '52', '53', '54', '55']:
             print('MASTERCARD\n')
+        else:
+            print('INVÁLIDO\n')
 
 
-# Validar o número do cartão
+# Função que valida o número do cartão
 def validate_card(num):
-    num = str(num)   # Conversão do número para String
-    pair_sum = 0     # Armazena os dígitos começando pelo penúltimo
-    odd_sum = 0      # Armazena os demais dígitos
+    # Declaração de variáveis para armazenar os dígitos pares (do penúltimo ao primeiro)
+    # e ímpares (demais) e conversão do número para String
+    num = str(num)
+    pair_sum = 0
+    odd_sum = 0
 
-    for i in range(len(num) - 2, -1, -2):   # Do penúltimo dígito ao primeiro (índex 0) com espaço de 2
-        if int(num[i]) * 2 > 9:             # Verifica se o número possui dois dígitos
-            converter = str(int(num[i]) * 2)[0:2]
-            pair_sum += int(converter[0]) + int(converter[1])
+    # Aqui está o Algoritmo de Luhn propriamente dito
+    # Soma o dobro dos dígitos do penúltimo ao primeiro
+    for i in range(len(num) - 2, -1, -2):
+        if int(num[i]) * 2 > 9:  # Verifica se o número possui dois dígitos e aplica a técnica de subtração por 9
+            pair_sum += int(num[i]) * 2 - 9
         else:
             pair_sum += int(num[i]) * 2
 
-    for i in range(len(num) - 1, -1, -2):   # Do último dígito ao primeiro (índex 0) com espaço de 2
-        if int(num[i]) * 2 > 9:             # Verifica se o número possui dois dígitos
-            converter = str(int(num[i]) * 2)[0:2]
-            odd_sum += int(converter[0]) + int(converter[1])
-        else:
-            odd_sum += int(num[i]) * 2
+    # Soma os demais dígitos e verifica se a soma é múltipla de 10
+    for i in range(len(num) - 1, -1, -2):
+        odd_sum += int(num[i])
 
     if (odd_sum + pair_sum) % 10 == 0:
         return True
@@ -36,7 +37,7 @@ def validate_card(num):
         return False
 
 
-card_num = int(input('Digite o número do cartão: '))
+card_num = int(input('Digite o número do cartão (apenas cartões Visa, American Express ou Mastercard): '))
 
 if validate_card(card_num):
     id_card(card_num)
